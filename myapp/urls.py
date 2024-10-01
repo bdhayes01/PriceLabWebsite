9b4dba816a08556d1e298e59ec2e0a13db3ff738
bdhayes01 <briandhayes01@gmail.com>
2024-10-01 15:06:11 -0600

    Added icon and silenced a warning

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
     path('upload_csv/', views.upload_csv, name='upload_csv'),
     path('make_cohorts/', views.make_cohorts, name='make_cohorts')
 ]
